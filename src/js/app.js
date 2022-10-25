f93b31fa6502c367f41dbdb0f55a51c9c64bfe90
Stark <serj.elessar@gmail.com>
2022-10-26 02:22:12 +0300

    fix: update files

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,32 @@ import * as flsFunctions from './modules'
 import { MousePRLX } from './libs/parallaxMouse'
 
 /* Раскомментировать для использования */
-// import Swiper, { Navigation, Pagination } from 'swiper'
+import Swiper, { Pagination } from 'swiper'
 
 // Запускаем и добавляем в объект модулей
 const mousePrlx = new MousePRLX({})
 
 flsFunctions.isWebp()
 flsFunctions.burgerMenu()
+flsFunctions.countDown(5, 'timer-podvyazka')
+flsFunctions.countDown(3, 'timer-domik')
+flsFunctions.countDown(15, 'timer-hranenie')
+flsFunctions.countDown(1, 'timer-pensiya')
+flsFunctions.countDown(30, 'timer-zacepy')
+flsFunctions.countDown(50, 'timer-dostavka')
+
+new Swiper('.promotions__slider', {
+  modules: [ Pagination ],
+  direction: 'horizontal',
+  autoplay: {
+    delay: 1000,
+  },
+  speed: 800,
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 65,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,10 +1,10 @@
 // FLS (Full Logging System)
 export function FLS(message) {
-	setTimeout(() => {
-		if (window.FLS) {
-			console.log(message);
-		}
-	}, 0);
+  setTimeout(() => {
+    if (window.FLS) {
+      console.log(message)
+    }
+  }, 0)
 }
 
 export function isWebp() {
@@ -14,9 +14,10 @@ export function isWebp() {
     webP.onload = webP.onerror = function () {
       callback(webP.height == 2)
     }
-    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+    webP.src =
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
   }
-  // Добавление класса _webp или _no-webp для HTML 
+  // Добавление класса _webp или _no-webp для HTML
   testWebp(function (support) {
     let className = support === true ? 'webp' : 'no-webp'
     document.documentElement.classList.add(className)
@@ -39,12 +40,35 @@ export function burgerMenu() {
     if (target && target === buttonPolicy) {
       document.querySelector('.policy').classList.add('_active')
     }
-    
+
     if (target && target.classList.contains('_overlay')) {
       document.querySelector('.policy').classList.remove('_active')
       buttonBurger.classList.remove('_active')
       burgerMenu.classList.remove('_active')
     }
-    
   })
+}
+
+export function countDown(hourNumber, timerId) {
+  const currentTimer = document.getElementById(timerId)
+  const hoursPlace = currentTimer.querySelector('.timer__group_hours')
+  const minutesPlace = currentTimer.querySelector('.timer__group_minutes')
+  const secondsPlace = currentTimer.querySelector('.timer__group_seconds')
+  const countDownDate = new Date().getTime() + hourNumber * 60 * 60 * 1000
+
+  const x = setInterval(function () {
+    const now = new Date().getTime()
+    const distance = countDownDate - now
+
+    // const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    hoursPlace.innerHTML = hours <= 9 ? `0${hours}` : hours
+    minutesPlace.innerHTML = minutes <= 9 ? `0${minutes}` : minutes
+    secondsPlace.innerHTML = seconds <= 9 ? `0${seconds}` : seconds
+
+    if (distance < 0) clearInterval(x)
+  }, 1000)
 }
